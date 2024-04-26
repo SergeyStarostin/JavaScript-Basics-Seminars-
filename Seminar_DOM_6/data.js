e219d411c168ff9c4c0991c6814f23d640f3e942
Y127908 <Sergey.starostin@vaz.ru>
2024-04-26 09:16:47 +0400

    Added task for seminar 6

---
+++ Seminar_DOM_6/data.js
@@ -0,0 +1,22 @@
+const DataJS = `[
+    {
+        "class":"cart",
+        "imgUrl":"https://img.freepik.com/free-photo/spring-wardrobe-switch-over-still-life_23-2150176772.jpg",
+        "name": "MANGO  PEOPLE  ",
+        "category":"T-SHIRT",
+        "price": "300$",
+        "color":"red",
+        "size":"XL",
+        "quantity":"1"
+    },
+    {
+        "class":"cart",
+        "imgUrl":"https://img.freepik.com/free-photo/fast-fashion-vs-slow-sustainable-fashion_23-2149133973.jpg",
+        "name": "ELLERY X M'O ",
+        "category":"CAPSULE",
+        "price": "52$",
+        "color":"black",
+        "size":"XL",
+        "quantity":"2"
+    }
+]`;
